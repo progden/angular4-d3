f6ae59c2f7463067cc18c2e438ff932ace393993
張家榮 <progden@PDs-MBP13.local>
2017-08-13 16:56:08 +0800

    port d3 example as a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { D3PaintComponent } from './d3-paint/d3-paint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    D3PaintComponent
   ],
   imports: [
     BrowserModule
